b3f76f229c2a4e31a7c36e5f8025705360a08c5b
cristinarangoe <cristinarangoe@hotmail.com>
2022-08-11 11:45:07 -0500

    pagina de inicio

---
+++ src/components/NavBarLogo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NavBarLogo() {
+  return (
+    <div>
+        <img src="/img/Logo.png" alt="netlix logo" className="w-20 md:w-32 lg:w-40 absolute left-6 top-5 md:left-10 "/>
+    </div>
+  )
+}
