046ceacfe1ec491bc4326621d8db7af90b4bb949
Cristina Arango Escobar <cristinarangoe@hotmail.com>
2022-08-12 01:29:06 +0000

    Inicio maqueta signin y setup de la navegacion

--- src/components/inicioNav.jsx
+++ src/components/inicioNav.jsx
@@ -1,13 +1,16 @@
 import NavBarLogo from "./NavBarLogo";
+import { Link } from "react-router-dom";
 
 export default function InicioNav() {
   return (
     <nav className="px-[20px] ">
       <div className="flex flex-row w-full justify-between pt-[0.5rem] items-center">
         <NavBarLogo />
-        <button className="text-white bg-red-600 py-[0.25rem] px-[0.5rem] text-[0.9rem] rounded-[3px]">
-          Iniciar sesion
-        </button>
+        <Link to="/signin">
+          <button className="text-white bg-red-600 py-[0.25rem] px-[0.5rem] text-[0.9rem] rounded-[3px]">
+            Iniciar sesion
+          </button>
+        </Link>
       </div>
     </nav>
   );
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import InicioNav from "../components/inicioNav";
 import RegisterForm from "../components/registerForm";
 
-export default function Inicio() {
+export default function SignInInicio() {
   return (
-    <div className="bg-[url('/img/ImgInicio.jpg')] bg-cover w-full h-full">
-      <InicioNav />
-      <main className="flex w-full h-full flex-row justify-center py-[50px] lg:items-center">
-        <div className="max-w-[640px] text-white text-center py-[30px] px-[5%]">
-          <h1 className="text-3xl">
-            Películas y series ilimitadas y mucho más.
-          </h1>
-          <h2 className="text-lg my-4">
-            Disfruta donde quieras. Cancela cuando quieras.
-          </h2>
-          <RegisterForm />
-        </div>
-      </main>
+    <div className="flex bg-[url('/img/ImgInicio.jpg')] bg-cover w-full h-full ">
+      <div className="inline-block bg-[rgba(0,0,0,.4)] h-full w-full z-0 absolute"></div>
+      <div className="z-10 w-full h-full">
+        <InicioNav />
+        <main className="flex w-full flex-row justify-center pt-[50px] lg:items-center ">
+          <div className="max-w-[640px] text-white text-center pt-[30px] px-[5%]">
+            <h1 className="text-3xl">
+              Películas y series ilimitadas y mucho más.
+            </h1>
+            <h2 className="text-lg my-4">
+              Disfruta donde quieras. Cancela cuando quieras.
+            </h2>
+            <RegisterForm />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
