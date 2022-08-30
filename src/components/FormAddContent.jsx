f7ac739a476fad2813c161ec015945e2d70eddeb
cristinarangoe <cristinarangoe@hotmail.com>
2022-08-30 18:14:04 -0500

    anadiendo otros inputs

--- src/components/FormAddContent.jsx
+++ src/components/FormAddContent.jsx
@@ -73,6 +73,39 @@ export default function FormAddContent() {
         className="w-full p-[10px] h-[48px] my-[10px] bg-gray-300 placeholder:text-black border border-gray-200 rounded-md"
         type="text"
       />
+      <label className="text-white ml-0 text-lg" htmlFor="duracion">Duración</label>
+      <input
+        placeholder="Duracion"
+        {...register("duracion", { required: true })}
+        className="w-full p-[10px] h-[48px] my-[10px] bg-gray-300 placeholder:text-black border border-gray-200 rounded-md"
+        type="text"
+      />
+      <label className="text-white ml-0 text-lg" htmlFor="Precio">Precio</label>
+      <input
+        placeholder="Precio"
+        {...register("Precio", { required: true })}
+        className="w-full p-[10px] h-[48px] my-[10px] bg-gray-300 placeholder:text-black border border-gray-200 rounded-md"
+        type="number"
+      />
+      <button className="min-h-[48px] px-[1em] py-[0.25em] rounded-[2px] bg-red-600 mt-[0.5em] text-center flex flex-row items-center text-white">
+            <span className="text-[1rem]">Adicionar</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={1}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </span>
+          </button>
     </form>
   );
 }
--- src/pages/AddSeries.jsx
+++ src/pages/AddSeries.jsx
@@ -4,12 +4,14 @@ import HomeNavBar from "../components/HomeNavBar";
 
 export default function () {
   return (
-    <div className="bg-black/90 h-full w-full">
-      <div className="relative z-10">
-        <HomeNavBar />
+    <div className="bg-[url('/public/img/ImgInicio.jpg')] bg-cover h-full w-full">
+      <div className="relative z-10 bg-black/80">
+        <HomeNavBar/>
       </div>
-      <div>
+      <div className="flex flex-col justify-center max-w-[640px] py-3 mx-5 md:mx-auto my-8 mb-auto bg-black/80">
+        <h2 className="text-white font-bold text-2xl text-center my-3">Que serie quisieras ver en nuestro portafolio?</h2>
         <FormAddContent/>
+        
       </div>
     </div>
   );
