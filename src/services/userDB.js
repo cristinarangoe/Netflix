03d88dc2b7481ecdac8eee0bc420d5db0e318fdc
fillsion <60821971+fillsion@users.noreply.github.com>
2022-09-01 14:12:04 -0500

    Si se pudo
    si se pudo

--- src/services/userDB.js
+++ src/services/userDB.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 
- const url = "https://cf5ag3fame.execute-api.us-east-1.amazonaws.com/Dev/user/?user="
+ const url = "https://cf5ag3fame.execute-api.us-east-1.amazonaws.com/Dev/user/"
 const userDBServices = {
 
     async getUserPlan(){
         console.log("breve")
         let user = {user: "prueba"}
-        const data = await axios.get(url + user.user)
+        const data = await axios.get(url, {params:{user:"prueba"}})
         console.log(data.data)
         
     }
